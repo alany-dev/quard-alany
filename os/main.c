613d60ecd14a1020758f8c35cd91d2c3f798c238
star-cs <2572523998@qq.com>
2026-03-01 15:29:49 +0800

    feat: quard_star 封装 printf 函数

--- os/main.c
+++ os/main.c
@@ -1,11 +1,6 @@
-extern void sbi_console_putchar(int ch);
+#include "os.h"
 
 void os_main()
 {
-    sbi_console_putchar('h');
-    sbi_console_putchar('e');
-    sbi_console_putchar('l');
-    sbi_console_putchar('l');
-    sbi_console_putchar('o');
-    sbi_console_putchar('!');
+    printf("hello alany os!");
 }
